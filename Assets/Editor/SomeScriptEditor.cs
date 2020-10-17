8339160a286791709634862d8b5d830064a99629
Dmitry Minsky <me@dminsky.com>
2020-10-17 23:56:04 +0300

    Draw default inspector.

---
+++ Assets/Editor/SomeScriptEditor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(SomeScript))]
+public class SomeScriptEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {        
+        base.OnInspectorGUI();
+        EditorGUILayout.HelpBox("This is a HelpBox", MessageType.Info);
+        // DrawDefaultInspector();
+    }
+}
