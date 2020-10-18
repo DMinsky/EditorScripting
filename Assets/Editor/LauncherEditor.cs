32a4857c0a5ca258d973cb54a37a00f43f6ba33d
Dmitry Minsky <me@dminsky.com>
2020-10-18 16:16:39 +0300

    SceneGUI button

--- Assets/Editor/LauncherEditor.cs
+++ Assets/Editor/LauncherEditor.cs
@@ -6,6 +6,17 @@ using UnityEditor;
 [CustomEditor(typeof(Launcher))]
 public class LauncherEditor : Editor
 {
+    private Vector3 screenPos;
+    private Rect rectResult;
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        EditorGUILayout.Vector3Field("Pos", screenPos);
+        EditorGUILayout.RectField("Result", rectResult);
+        
+    }
+
     void OnSceneGUI()
     {
         var launcher = target as Launcher;
@@ -16,5 +27,25 @@ public class LauncherEditor : Editor
                 transform.rotation
             )
         );
+
+        Handles.BeginGUI();
+
+        screenPos = Camera.current.WorldToScreenPoint(
+            launcher.transform.position + launcher.offset
+        );
+        // 2 from retina screen?
+        // SceneView.currentDrawingSceneView.position includes window header
+        rectResult.xMin = screenPos.x / 2f;
+        rectResult.yMin = (SceneView.currentDrawingSceneView.position.height * 2f - screenPos.y) / 2f;
+        rectResult.width = 64;
+        rectResult.height = 20;
+        GUILayout.BeginArea(rectResult);
+        using (new EditorGUI.DisabledGroupScope(!Application.isPlaying)) {
+            if (GUILayout.Button("Fire")) {
+                launcher.Fire();
+            }
+        }
+        GUILayout.EndArea();
+        Handles.EndGUI();
     }
 }
