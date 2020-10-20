5409771b6105c793589790e2e8cb2bb0094f4130
Dmitry Minsky <me@dminsky.com>
2020-10-20 19:42:06 +0300

    Gizmos

---
+++ Assets/Scripts/Relic/Editor/RelicEditor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor( typeof( Relic ) )]
+public class RelicEditor : Editor
+{
+    [DrawGizmo( GizmoType.Active | GizmoType.NonSelected | GizmoType.Pickable )]
+    private static void DrawTypeGizmo( Relic relic, GizmoType gizmoType )
+    {
+        string iconPath = "Relics/" + relic.relicType + ".png";
+        Gizmos.DrawIcon( relic.transform.position, iconPath );
+    }
+
+    [DrawGizmo( GizmoType.Active | GizmoType.NonSelected )]
+    private static void DrawHPGizmo( Relic relic, GizmoType gizmoType )
+    {
+        string hpLabel = "HP: " + relic.currHP + "/" + relic.maxHP;
+        Handles.Label(
+            relic.transform.position + Vector3.up * 0.125f,
+            hpLabel,
+            GUI.skin.FindStyle("AssetLabel")            
+        );
+    }
+
+    [DrawGizmo( GizmoType.NonSelected )]
+    private static void DrawTriggerZone( Relic relic, GizmoType gizmoType )
+    {
+        BoxCollider collider = relic.GetComponent<BoxCollider>();
+        Gizmos.color = Color.black;
+        Gizmos.matrix = relic.transform.localToWorldMatrix;
+        Gizmos.DrawWireCube( Vector3.zero, collider.size );
+    }
+
+}
---
+++ Assets/Scripts/Relic/Relic.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Relic : MonoBehaviour
+{
+    public float currHP = 1.5f;
+    public int maxHP = 10;
+    
+    public enum RelicType
+    {
+        Heartbreaker,
+        MaskedSpider,
+        ScorpionTail,
+        BladedPinchers
+    }
+
+    public RelicType relicType;
+
+    // void OnDrawGizmos()
+    // {
+    //     string customName = "Relics\\" + relicType + ".png";
+    //     Gizmos.DrawIcon( transform.position, customName, true );
+    // }
+}
