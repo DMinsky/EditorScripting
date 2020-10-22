7cfa52b6cb0c018714ab99a15bc6398c6297088a
Dmitry Minsky <me@dminsky.com>
2020-10-22 16:30:21 +0300

    ScriptableWizard

---
+++ Assets/Editor/SelectAllOfTag.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class SelectAllOfTag : ScriptableWizard
+{
+    public string searchTag = "hui";
+
+    [MenuItem("Tools/Select All of Tag")]
+    static void SelectAllOfTagWizard()
+    {
+        ScriptableWizard.DisplayWizard<SelectAllOfTag>("Select all of tag", "Make Selection");
+    }
+
+    void OnWizardCreate()
+    {
+        GameObject[] gameObjects = GameObject.FindGameObjectsWithTag(searchTag);
+        Selection.objects = gameObjects;
+    }
+
+    void OnWizardUpdate()
+    {
+        helpString = "Please set the color of the light!";
+    }
+}
